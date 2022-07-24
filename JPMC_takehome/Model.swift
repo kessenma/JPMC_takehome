55400041785cb3cb1e518cf7b06aff29d11e887f
Kyle Essenmacher <kessenmacher7832@gmail.com>
2022-07-24 16:10:58 -0400

    transferred code

---
+++ JPMC_takehome/Model.swift
@@ -0,0 +1,82 @@
+//
+//  Model.swift
+//  NYC SCHOOLS + SAT SCORES
+
+//
+//  Created by Kyle Essenmacher on 7/24/22.
+//
+
+import Foundation
+
+
+
+//struct ToDo:Decodable {
+//    let userId: Int
+//    let id: Int
+//    let title:String
+//    let completed:Bool
+//
+//
+//}
+
+struct ToDo:Decodable {
+    let dbn: String
+    let school_name: String
+    let num_of_sat_test_takers:String
+    let sat_critical_reading_avg_score:String
+    let sat_math_avg_score:String
+    let sat_writing_avg_score:String
+}
+
+
+//struct ToDo:Decodable {
+//    let dbn: String
+//    let school_name: String
+//    let num_of_sat_test_takers:String
+//    let sat_critical_reading_avg_score:String
+//    let sat_math_avg_score:String
+//    let sat_writing_avg_score:String
+//    var isOpened: Bool = false
+//
+//    init(dbn: String,
+//         school_name: String,
+//         num_of_sat_test_takers:String,
+//         sat_critical_reading_avg_score:String,
+//         sat_math_avg_score:String,
+//         sat_writing_avg_score:String,
+//         isOpened: Bool = false
+//    ) {
+//        self.dbn = dbn
+//        self.school_name = school_name
+//        self.num_of_sat_test_takers = num_of_sat_test_takers
+//        self.sat_critical_reading_avg_score = sat_critical_reading_avg_score
+//        self.sat_math_avg_score = sat_math_avg_score
+//        self.sat_writing_avg_score = sat_writing_avg_score
+//
+//        self.isOpened = isOpened
+//    }
+//}
+
+
+//
+
+
+//struct ToDo:Decodable {
+//    let dbn: String
+//    let school_name: String
+//    let num_of_sat_test_takers:String
+//    let sat_critical_reading_avg_score:String
+//    let sat_math_avg_score:String
+//    let sat_writing_avg_score:String
+//    let options: [String]
+//    var isOpened: Bool = false
+//
+//    init(school_name: String,
+//         options: [String],
+//         isOpened: Bool = false
+//    ) {
+//        self.school_name = school_name
+//        self.options = options
+//        self.isOpened = isOpened
+//    }
+//}
--- JPMC_takehome/ViewController.swift
+++ JPMC_takehome/ViewController.swift
@@ -1,19 +1,106 @@
 //
 //  ViewController.swift
-//  JPMC_takehome
-//
+//  NYC SCHOOLS + SAT SCORES
+//  data: https://data.cityofnewyork.us/resource/f9bf-2cp4.json
 //  Created by Kyle Essenmacher on 7/24/22.
 //
 
 import UIKit
+import SwiftUI
 
 class ViewController: UIViewController {
-
+    
+    var data = [ToDo]()
+    
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // Method Calling
+        
+        fetchingApiData(URL: "https://data.cityofnewyork.us/resource/f9bf-2cp4.json") { result in
+            self.data = result
+            DispatchQueue.main.async {
+                self.tableView.reloadData()
+            }
+        }
     }
-
-
+        
+    func fetchingApiData(URL url: String, completion: @escaping ([ToDo]) -> Void) {
+                             
+            let url = URL(string: url)
+            let session = URLSession.shared
+            let dataTask = session.dataTask(with: url!) { data, response, error in
+                if data != nil && error == nil {
+                do {
+                    let parsingData = try JSONDecoder().decode ([ToDo].self, from: data!)
+                    completion(parsingData)
+                }catch {
+                    print ("Parsing error")
+                }
+                
+            }
+        }
+            dataTask.resume()
+    }
 }
+
+extension ViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return data.count
+    }
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "cell")
+        cell?.textLabel?.text = data[indexPath.row].school_name
+        return cell!
+    }
+    
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//class ViewController: UIViewController {
+//
+//    override func viewDidLoad() {
+//        super.viewDidLoad()
+//
+//        // Method Calling
+//
+//        fetchingApiData(URL: "https://jsonplaceholder.typicode.com/todos") {result in
+//            print(result)
+//        }
+//    }
+//
+//    func fetchingApiData(URL url:String, completion: @escaping ([ToDo]) -> Void) {
+//
+//        let url = URL(string: url)
+//        let session = URLSession.shared
+//        let dataTask = session.dataTask(with: url!) { data, response, error in
+//            if data != nil && error == nil {
+//                do {
+//                let parsingData = try JSONDecoder().decode([ToDo].self, from: data!)
+//                    completion([parsingData])
+//                }catch {
+//                    print ("Parsing erorr")
+//                }
+//
+//            }
+//        }
+//        dataTask.resume()
+//    }
+//}
 
